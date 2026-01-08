09c358795ac9ddd76ecaf95cc67b7b967150a8c9
tinilinsk <tema.ryabkov@gmail.com>
2026-01-08 11:57:15 +0100

    Added functionality for AddProduct page

--- EasyHome/Views/AddProduct.xaml.cs
+++ EasyHome/Views/AddProduct.xaml.cs
@@ -6,4 +6,14 @@ public partial class AddProduct : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void SubmitNewProduct(object sender, EventArgs e)
+	{
+		string name = InputName.Text;
+		string description = InputDescription.Text;
+		decimal price = decimal.Parse(InputPrice.Text);
+		bool inStock = InputInStock.IsToggled;
+
+		Result.Text = $"Product Added:\nName: {name}\nDescription: {description}\nPrice: {price:C}\nIn Stock: {inStock}";
+    }
 }
