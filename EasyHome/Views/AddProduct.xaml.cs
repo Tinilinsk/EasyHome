68e531932adb944db54a53971f17c647a5c39402
tinilinsk <tema.ryabkov@gmail.com>
2026-01-08 12:04:39 +0100

    Added walidation for form in AddProduct page

--- EasyHome/Views/AddProduct.xaml.cs
+++ EasyHome/Views/AddProduct.xaml.cs
@@ -7,11 +7,38 @@ public partial class AddProduct : ContentPage
 		InitializeComponent();
 	}
 
-	private void SubmitNewProduct(object sender, EventArgs e)
+	private async void SubmitNewProduct(object sender, EventArgs e)
 	{
-		string name = InputName.Text;
+        if (string.IsNullOrWhiteSpace(InputName.Text))
+        {
+            await DisplayAlert("Error", "Please enter product name", "OK");
+            InputName.Focus();
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(InputDescription.Text))
+        {
+            await DisplayAlert("Error", "Please enter category name", "OK");
+            InputDescription.Focus();
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(InputPrice.Text))
+        {
+            await DisplayAlert("Error", "Please enter product price", "OK");
+            InputPrice.Focus();
+            return;
+        }
+
+        if (!decimal.TryParse(InputPrice.Text.Replace('.', ','), out decimal price) || price < 0)
+        {
+            await DisplayAlert("Error", "Please enter a valid positive number for price", "OK");
+            InputPrice.Text = "";
+            InputPrice.Focus();
+            return;
+        }
+        string name = InputName.Text;
 		string description = InputDescription.Text;
-		decimal price = decimal.Parse(InputPrice.Text);
 		bool inStock = InputInStock.IsToggled;
 
 		Result.Text = $"Product Added:\nName: {name}\nDescription: {description}\nPrice: {price:C}\nIn Stock: {inStock}";
