ff44f721cf58c8758518c51d93aeae540c54b2be
tinilinsk <tema.ryabkov@gmail.com>
2026-01-09 09:32:09 +0100

    Changed modul Product

--- EasyHome/Models/Products.cs
+++ EasyHome/Models/Products.cs
@@ -8,8 +8,10 @@ namespace EasyHome.Models
     {
         [SQLite.PrimaryKey, SQLite.AutoIncrement]
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public bool InStock { get; set; }
     }
